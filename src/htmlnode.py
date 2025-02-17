0c763fa7672b06740bc55317c3ec11f1c94b0458
rhsnake <rhsnake@gmail.com>
2025-02-17 23:19:41 +0800

    added parent node, added function in Textnode to convert textnodes into html nodes

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -38,5 +38,19 @@ class LeafNode(HTMLNode):
         return f"LeafNode({self.tag}, {self.value}, {self.props})"
     
         
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children,  props=None):
+        super().__init__(tag, None, children, props)
     
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("Invalid HTML: No Tag")
+        if self.children is None:
+            raise ValueError("Invalid HTML: No Children")
+        child_output = ""
+        for child in self.children:
+            child_output += child.to_html()
+        return f'<{self.tag}{self.props_to_html()}>{child_output}</{self.tag}>'
     
+    def __repr__(self):
+        return f"ParentNode({self.tag}, children: {self.children}, {self.props})"

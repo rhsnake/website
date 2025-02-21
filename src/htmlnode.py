403f012eb7f84deeac880d12fffebade862295d4
= <=>
2025-02-21 20:47:53 +0800

    g

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,5 +1,3 @@
-
-
 class HTMLNode:
     
     def __init__(self,tag = None,value = None,children = None,props = None):
@@ -15,11 +13,24 @@ class HTMLNode:
         output = ""
         for prop in self.props:
             value = self.props[prop]
-            output += f'{prop}="{value}" '
+            output += f' {prop}="{value}"'
         return output
     
     def __repr__(self):
         return  f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
         
+class LeafNode(HTMLNode):
     
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
     
+    def to_html(self):
+        if self.value is None:
+            raise ValueError ("No value")
+        if self.tag is None:
+            return self.value
+        
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+    
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"

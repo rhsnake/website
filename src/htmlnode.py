c30fea9ce93fa60f643b9389c05ca8ac9afdf64b
= <=>
2025-04-28 16:22:49 +0800

    added inline_markdown and unit tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -20,6 +20,35 @@ class HTMLNode:
     
     def __repr__(self):
         return  f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
+    
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("invalid HTML: no value")
+        if self.tag is None:
+            return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
+    
         
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children,  props=None):
+        super().__init__(tag, None, children, props)
     
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("Invalid HTML: No Tag")
+        if self.children is None:
+            raise ValueError("Invalid HTML: No Children")
+        child_output = ""
+        for child in self.children:
+            child_output += child.to_html()
+        return f'<{self.tag}{self.props_to_html()}>{child_output}</{self.tag}>'
     
+    def __repr__(self):
+        return f"ParentNode({self.tag}, children: {self.children}, {self.props})"
